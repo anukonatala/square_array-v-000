524e29e8c13886b337e5a964d7e0517efcc9a891
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 01:41:26 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   # your code here
   squareArray =[]
-  array.each {|x| squareArray.push(x*x) }
+  #array.each {|x| squareArray.push(x*x) }
+  array.collect { |x| x * x }  
   squareArray
 end
